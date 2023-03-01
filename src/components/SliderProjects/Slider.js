bf45780515535ce892fc715371137af45470c7ed
sanajani <webdevfarsi@gmail.com>
2023-03-01 21:59:48 +0430

    slider part is finish

--- src/components/SliderProjects/Slider.js
+++ src/components/SliderProjects/Slider.js
@@ -4,10 +4,32 @@ import { useState } from 'react'
 
 const Slider = ({slides}) => {
   const [currentIndex, setCurrentIndex] = useState(0)
+
+
+
+  const nextSlide = () => {
+    const nextSlides = currentIndex === slides.length -1
+    const newIndex = nextSlides ? 0 : currentIndex + 1
+    setCurrentIndex(newIndex)
+  }
+  const preSlide = () => {
+    const preSlides = currentIndex === 0
+    const newIndex = preSlides ? slides.length -1 : currentIndex - 1
+    setCurrentIndex(newIndex)
+  }
+
+
+  const sliderContainerStyle = {
+    backgroundImage:`url(${slides[currentIndex].imgUrl})`
+  }
+
   return (
     <div className='slider'>
-    <div style={{backgroundImage:`url(${slides[currentIndex].imgUrl})`}} className="sliderContainer">
-
+    <div style={sliderContainerStyle} className="sliderContainer">
+      <h1>{slides[currentIndex].name}</h1>
+      <p>{slides[currentIndex].title}</p>
+        <span className='preSlide' onClick={preSlide}><img src="./assets/backwordarrow.png" alt="" /></span>
+        <span className='nextSlide' onClick={nextSlide}><img src="./assets/forwordarrow.png" alt="" /></span>
     </div>
     </div>
   )
